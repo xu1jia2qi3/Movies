764e985cf228c906b2cfc6fafe98d8e702f90216
xu1jia2qi3 <t54yang@uwaterloo.ca>
2018-09-23 11:53:18 -0400

    correct

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # movies
+
+To use this project you must to have python installed
+
+1 Download the code or clone the repository
+2.click entertainment_center.py
+3.click Run entertainment-center.py
+4.chose chrome (or other browser you have)
+5.enjoy the trailers!
--- media.py
+++ media.py
@@ -3,12 +3,23 @@ import webbrowser
 
 # def a class
 class Movie:
-    def __init__(self, movie_title, movie_storyline,poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+        # self.title is the name of the movie
+        # self.storyline is the brief sum of this movie
+        # self.poster_image is the poster
+        # self.trailer_youtube_url is the link from youtube
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-# def function for opening trailer
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+
+"""
+def function for opening trailer that when user click
+movies,which shows trailer from youtube
+"""
+
+
+def show_trailer(self):
+    webbrowser.open(self.trailer_youtube_url)
